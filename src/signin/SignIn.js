2f0fe16a6382ca5c5fd590f9b5ad6fdd996d8a9d
Kseniia Shavonina <kshavonina@yandex.ru>
2019-02-13 01:54:21 +0300

    First dirty, but working solution for retrieving access token from backend

--- src/signin/SignIn.js
+++ src/signin/SignIn.js
@@ -5,17 +5,51 @@ class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: ''
+      grant_type: 'password',
+      username: '',
+      password: '',
+      client_id: 'trusted-client',
+      client_secret: 'secret'
     }
   }
 
   signIn() {
-    fetch('http://localhost:8080/budgetcheckerweb/users/test', {
+    const formData = new URLSearchParams();
+    formData.append('grant_type', this.state.grant_type);
+    formData.append('username', this.state.username);
+    formData.append('password', this.state.password);
+
+    fetch('http://localhost:8080/budgetcheckerweb/oauth/token', {
       method: 'POST',
-      mode: 'cors',
-      body: JSON.stringify(this.state)
-    }).then(res => console.log(res));
+      //mode: 'cors',
+      headers: {
+        //'Authorization': 'Basic ' + btoa(encodeURI(this.client_id) + ':' + encodeURI(this.client_secret)),
+        'Authorization': 'Basic dHJ1c3RlZC1jbGllbnQ6c2VjcmV0',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        //'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formData.toString()
+        //JSON.stringify(this.state)
+    })
+      .then(
+        function(response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+
+          // Examine the text in the response
+          response.json().then(function(data) {
+            console.log(data);
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
   }
 
   render() {
@@ -26,7 +60,7 @@ class SignIn extends Component {
           className="input"
           type="text"
           placeholder="email"
-          onChange={event => this.setState({email: event.target.value})}
+          onChange={event => this.setState({username: event.target.value})}
         />
         <input
           className="input"
